7c9fcbdc3c0e3926f1cda53566b22d2d30b6ce03
suguz06 <drselmanuguz@gmail.com>
2020-09-12 16:24:17 -0500

    day4

--- src/test/java/com/cybertek/tests/day4_findElements_chechBoxes_radio/P3_CheckedBox_Practice.java
+++ src/test/java/com/cybertek/tests/day4_findElements_chechBoxes_radio/P3_CheckedBox_Practice.java
@@ -22,24 +22,23 @@ public class P3_CheckedBox_Practice {
 
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
 
         driver.get("http://practice.cybertekschool.com/checkboxes");
 
 
-
-WebElement checkBox1=driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]"));
-WebElement checkBox2=driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]"));
+        WebElement checkBox1 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]"));
+        WebElement checkBox2 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]"));
 
         //2.Confirm	checkbox	#1	is	NOT	selected	by	default
-if(!checkBox1.isSelected()) {
-    System.out.println("Checkbox is not selected. Verification is PASSED!");
-} else {
-    System.out.println("Checkbox is   selected. Verification is FAILED!!!");
-}
+        if (!checkBox1.isSelected()) {
+            System.out.println("Checkbox is not selected. Verification is PASSED!");
+        } else {
+            System.out.println("Checkbox is   selected. Verification is FAILED!!!");
+        }
 
 //3.Confirm	checkbox	#2	is	SELECTED	by	default.
-        if(checkBox2.isSelected()) {
+        if (checkBox2.isSelected()) {
             System.out.println("Checkbox is   selected. Verification is PASSED!");
         } else {
             System.out.println("Checkbox is  not selected. Verification is FAILED!!!");
@@ -47,18 +46,18 @@ if(!checkBox1.isSelected()) {
 
 
 //4.Click	checkbox	#1	to	select	it.
-         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-       Thread.sleep(1000);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Thread.sleep(1000);
         checkBox1.click();
 
 
 //5.Click	checkbox	#2	to	deselect	it.
         Thread.sleep(1000);
-checkBox2.click();
+        checkBox2.click();
 
 
 //6.Confirm	checkbox	#1	is	SELECTED.
-        if(checkBox1.isSelected()) {
+        if (checkBox1.isSelected()) {
             System.out.println("Checkbox is   selected. Verification is PASSED!");
         } else {
             System.out.println("Checkbox is  not selected. Verification is FAILED!!!");
@@ -67,12 +66,26 @@ checkBox2.click();
 
 //7.Confirm	checkbox	#2	is	NOT
 
-        if(!checkBox2.isSelected()) {
+        if (!checkBox2.isSelected()) {
             System.out.println("Checkbox is  not selected. Verification is PASSED!");
         } else {
             System.out.println("Checkbox is    selected. Verification is FAILED!!!");
         }
 
+
+        WebElement homeLink = driver.findElement(By.xpath("//a[.='Home']"));
+
+        homeLink.click();
+
+        Thread.sleep(1000);
+
+        driver.navigate().back();
+        Thread.sleep(1000);
+
+// after navigations ==> loose connection between driver ==> then refresh the locators
+ checkBox1 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]"));
+
+        checkBox1.click();
 
 
     }
